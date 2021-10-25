4b223836b51b58f6b3df9faa30a2339fc8a08d13
Marc <marcbaque1311@gmail.com>
2021-10-25 23:08:05 +0200

    [ UPDATE ] bump deps

--- deps.ts
+++ deps.ts
@@ -14,7 +14,7 @@ export type { RouterContext } from 'https://deno.land/x/oak@v9.0.1/mod.ts';
 export { oakCors } from 'https://deno.land/x/cors@v1.2.2/mod.ts';
 export { SmtpClient } from 'https://deno.land/x/smtp@v0.7.0/mod.ts';
 
-export { MongoClient, Bson } from 'https://deno.land/x/mongo@v0.27.0/mod.ts';
+export { MongoClient, Bson } from 'https://deno.land/x/mongo@v0.28.0/mod.ts';
 
 export * as bcrypt from 'https://deno.land/x/bcrypt@v0.2.4/mod.ts';
 
