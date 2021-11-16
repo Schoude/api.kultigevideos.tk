ecd67e304816fd29d608b61217d6a4371dc496fb
Marc <marcbaque1311@gmail.com>
2021-11-16 23:19:58 +0100

    [ UPDATE ] change sorting criteria of listed videos

--- src/handlers/video.ts
+++ src/handlers/video.ts
@@ -131,7 +131,7 @@ export async function getVideosPanel(c: Context) {
       },
       {
         "$sort": {
-          "uploadedAt": -1,
+          "approvedAt": -1,
         },
       },
       ...createUserLookup("uploader"),
